6b194adfd13c25f78171a19fff0a2ae7d2f46a14
Jan Gradwohl <59826807+JanGradwohl@users.noreply.github.com>
2024-02-18 16:57:42 +0100

    Create CIBRA.h

---
+++ CIBRA/CIBRA.h
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+#ifndef CIBRA_H
+#define CIBRA_H
+
+void SendKeys(const char* keys);
+void SetMousePosition(int x, int y);
+
+char* os(const char* command);
+
+
+
+
+#endif
